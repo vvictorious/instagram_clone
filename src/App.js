8fa3e01511bb7dd3d7aee3ca15e20124404bc3f4
vvictorious <vvictorious30@gmail.com>
2021-01-04 00:44:59 -0700

    navbar is starting to come together

--- src/App.js
+++ src/App.js
@@ -2,8 +2,14 @@ import './App.css';
 
 function App() {
   return (
-    <div className="App">
-      <h1>Hi</h1>
+    <div className="app">
+      <div className='app__header'>
+        <img 
+          className='app__headerImage'
+          src='https://www.instagram.com/static/images/web/mobile_nav_type_logo.png/735145cfe0a4.png'
+          alt=''
+        /> 
+      </div>
     </div>
   );
 }
